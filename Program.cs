c6ec8fc1e431773d471bfb75bfd8425e92e6399d
at-elcapitan <gameforceman1808@gmail.com>
2024-02-15 13:04:58 +0200

    0.1: created size displaying, flags vars

--- Program.cs
+++ Program.cs
@@ -8,48 +8,43 @@ namespace DirectoryDisplay_sharp
     {
         private static string relative_path = "";
         private static string absolute_path = "";
+        private static bool sizeFlag;
+        private static bool allFlag;
 
         private static void SetRelativePath(string arg, bool absolute = false)
         {
             string[] path_arr;
 
-            arg = arg.TrimEnd('/');
-            path_arr = arg.Split('/');
+            arg = arg.TrimEnd('\\');
+            path_arr = arg.Split('\\');
 
             if (absolute & arg.Length > 1)
             {
-                relative_path = "/" + path_arr[path_arr.Length - 1] + "/";
+                relative_path = "\\" + path_arr[path_arr.Length - 1] + "\\";
             }
 
-            relative_path = path_arr[path_arr.Length - 1] + "/";
+            relative_path = path_arr[path_arr.Length - 1] + "\\";
         }
 
         public static void Main(string[] args)
         {
-            //  0   0  0
-            // SIZ ACC ALL 
-            int flags = 0;
 
             foreach (string arg in args)
             {
                 switch (arg)
                 {
                     case "--size":
-                        flags = flags | 0b100;
-                        break;
-
-                    case "--access":
-                        flags = flags | 0b010;
+                        sizeFlag = true;
                         break;
 
                     case "--all":
-                        flags = flags | 0b001;
+                        allFlag = true;
                         break;
 
                     default:
                         if (!arg.StartsWith("-", StringComparison.Ordinal))
                         {
-                            SetRelativePath(arg, arg.StartsWith("/", 
+                            SetRelativePath(arg, arg.StartsWith("\\", 
                                             StringComparison.Ordinal));
                             absolute_path = Path.GetFullPath(arg);
                             break;
@@ -60,15 +55,11 @@ namespace DirectoryDisplay_sharp
                            switch (arg[i])
                             {
                                 case 's':
-                                    flags = flags | 0b100;
-                                    break;
-
-                                case 'A':
-                                    flags = flags | 0b010;
+                                    sizeFlag = true;
                                     break;
 
                                 case 'a':
-                                    flags = flags | 0b001;
+                                    allFlag = true;
                                     break;
                             }
                         }
@@ -91,35 +82,101 @@ namespace DirectoryDisplay_sharp
                 }
             }
 
-            Console.WriteLine("{0}\n", relative_path);
+            Console.WriteLine(relative_path);
             var files = Directory.GetFiles(absolute_path)
-                        .Select(Path.GetFileName);
+                        .Select(Path.GetFileName)
+                        .ToArray<string>();
             string[] dirs = Directory.GetDirectories(absolute_path);
+
+            Array.Sort(files);
+            Array.Sort(dirs);
 
             foreach (string dir in dirs)
             {
                 string fdir = dir.Replace(absolute_path, "");
-                if (!fdir.StartsWith("/", StringComparison.Ordinal))
+                var dinfo = new DirectoryInfo(fdir);
+
+                /*if (dinfo.Attributes.HasFlag(FileAttributes.Hidden) && !allFlag)
                 {
-                    fdir = fdir.Insert(0, "/");
+                    continue;
+                }*/
+
+                if (!fdir.StartsWith("\\", StringComparison.Ordinal))
+                {
+                    fdir = fdir.Insert(0, "\\");
                 }
 
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("DIR\t");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                if (sizeFlag) Console.Write("DIR\t-\t- ");
+                else Console.Write("DIR\t- ");
+
+                /*if (dinfo.Attributes.HasFlag(FileAttributes.Hidden) && allFlag)
+                { 
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine(fdir);
+                    continue;
+                }*/
+
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine(fdir);
             }
 
             foreach (string file in files)
             {
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("FILE\t");
+                var finfo = new FileInfo(file);
+                long size = new FileInfo(file).Length;
 
-                if (file.Contains(".exe") || file.Contains(".com") || file.Contains(".bat"))
+                if (finfo.Attributes.HasFlag(FileAttributes.Hidden) && !allFlag)
+                {
+                    continue;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("FILE");
+
+                if (sizeFlag)
+                {
+                    string type = "B";
+
+                    if (size > 1000)
+                    {
+                        size /= 1024;
+                        type = "KB";
+                    }
+                    else if (size > 1000000)
+                    {
+                        size /= 1024;
+                        type = "MB";
+                    }
+                    else if (size > 1000000000)
+                    {
+                        size /= 1024;
+                        type = "GB";
+                    }
+
+                    if (sizeFlag)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("{0,5} {1}", size, type);
+                    }
+                }
+                
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\t- ");
+
+                if (finfo.Attributes.HasFlag(FileAttributes.Hidden) && allFlag)
+                    Console.ForegroundColor = ConsoleColor.Gray;
+
+                if (file.EndsWith(".exe") || file.EndsWith(".com") || file.EndsWith(".bat"))
                 {
                     Console.Write("*");
-                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    if (Console.ForegroundColor == ConsoleColor.Gray)
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    else Console.ForegroundColor = ConsoleColor.Green;
                 }
+
                 Console.WriteLine(file);
             }
         }
